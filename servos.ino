8f5df883b296986ed2581f7ae05ae9a97bd8e316
odonne36 <92492005+odonne36@users.noreply.github.com>
2023-02-26 01:46:35 -0500

    Add files via upload

--- servos.ino
+++ servos.ino
@@ -17,18 +17,22 @@ double time;
 Servo servobase;
 Servo servoarm;
 Servo servorotate;
-
+Servo spoontilt;
 
 void setup() {
   // put your setup code here, to run once:
   servobase.attach(3);
-  servobase.write(90);
+  servobase.write(0);
 
   servoarm.attach(5);
-  servoarm.write(90);
+  servoarm.write(0);
 
   servorotate.attach(6);
-  servorotate.write(90);
+  servorotate.write(0);
+
+  spoontilt.attach(9);
+  spoontilt.write(0);
+
 
 Serial.begin(9600);
 pinMode(7,OUTPUT);  //Trig
@@ -42,13 +46,25 @@ void loop() {
 
   // put your main code here, to run repeatedly:
 
-  servoarm.write(120);  // Go down to bowl level
+for (int i =0; i < 120; i++) {
+  servoarm.write(i);  // Go down to bowl level
+  delay(10);
+}
   delay(2000);
-  servorotate.write(45);  //Turn spoon back
+  for (int i =0; i < 45; i++) {
+  spoontilt.write(i); //Tilt spoon down
+  servorotate.write(i);  //Turn spoon back
+  delay(10);
+  }
   delay(2000);
-  servorotate.write(5);  // Turn spoon back upright
+  for (int i =0;i <90;i++) {
+  servorotate.write(i);  // Turn spoon back upright
+  spoontilt.write(i);    //Tilt spoon up
+  delay(10);
+  }
   delay(2000);
 
+//Polling ultrasonic sensor
 digitalWrite(7,LOW);
 delayMicroseconds(2);
 digitalWrite(7,HIGH);
@@ -71,11 +87,15 @@ time = pulseIn(8,HIGH);
 dist = time * 0.0133 / 2; //Inches
 }
   delay(2000);
-  servobase.write(60);
-  delay(7000);  //Move spoon to mouth and wait for 7s
+  for (int i = 0; i < 60;i++) {
+  servobase.write(i); //Move spoon to mouth and wait for 7s
+  delay(15);
+  }
+  delay(7000);  
 
 
   //Reset all to inital position and start again
+  
   servobase.write(5);
   servoarm.write(5);
   servorotate.write(5);
